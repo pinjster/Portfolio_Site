a5c74bd0eb5e7756766938712ff30d720cb60c66
pinjster <pinj2000@gmail.com>
2023-10-20 15:48:29 -0400

    added contact info

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 function page() {
   return (
     <div>
-        <h1>My Contact Information</h1>
+        <h4>My Contact Information</h4>
+        <p>Email: pinj2000@gmail.com</p>
+        <p>Phone no: +1(740)-274-0016</p>
+        <p>LinkedIn: <a href="https://www.linkedin.com/in/benjamin-copen/">here</a></p>
+        <p>GitHub: <a href="https://github.com/pinjster">here</a></p>
     </div>
   )
 }
