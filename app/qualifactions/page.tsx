f882dbee0cd22efe6b85ee203bccba3cd5258928
pinjster <pinj2000@gmail.com>
2023-10-30 22:54:24 -0400

    added headers to qualifications page

--- app/qualifactions/page.tsx
+++ app/qualifactions/page.tsx
@@ -4,6 +4,11 @@ function page() {
   return (
     <div>
         <h1>My Qualifications</h1>
+        <h4>Arizona State University</h4>
+        <h4>Coding Temple</h4>
+        <br />
+        <h1>Working Experience</h1>
+        <h4>Carvana</h4>
     </div>
   )
 }
