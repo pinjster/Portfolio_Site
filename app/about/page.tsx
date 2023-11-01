4f1f1cecddb546971737429d1d956a22d1e3095b
pinjster <pinj2000@gmail.com>
2023-10-31 22:38:03 -0400

    additions in about me page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,8 @@
 export default function page() {
     return (
         <div>
-            
+            <h1>About Me</h1>
+            <p>Hi, my name is Benjamin Copen</p>
         </div>
     )
 };
