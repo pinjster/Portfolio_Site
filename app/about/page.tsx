bd4e296de7e6f034e23259cd0ed2bbad26299b21
pinjster <pinj2000@gmail.com>
2023-11-03 16:55:04 -0400

    additions to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,8 +3,9 @@ export default function page() {
         <div>
             <h1>About Me</h1>
             <p>Hi, my name is Benjamin Copen</p>
-            <p>My goals:</p>
-            <p>My hobbies:</p>
+            <p>For the past three yers, I've been pursuing my career as a software developer.
+                I gained my certificate of Full-Stack Developer from Coding Temple in August, 2023.
+            </p>
         </div>
     )
 };
