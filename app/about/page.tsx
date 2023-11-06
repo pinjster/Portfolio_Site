85dcd4d7a8ae5adea8f3f48bf7835d7cbf956766
pinjster <pinj2000@gmail.com>
2023-11-05 21:35:39 -0500

    added goals p in about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,13 @@
 export default function page() {
     return (
         <div>
-            <h1>About Me</h1>
-            <p>Hi, my name is Benjamin Copen</p>
+            <h1>Hi, my name is Benjamin Copen</h1>
+            <h3>About Me</h3>
             <p>For the past three yers, I've been pursuing my career as a software developer.
                 I gained my certificate of Full-Stack Developer from Coding Temple in August, 2023.
             </p>
+            <h3>My Goals</h3>
+            <p>I intend to gain a bachelor's degree computer science.</p>
         </div>
     )
 };
