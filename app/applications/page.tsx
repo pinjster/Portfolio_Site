93ca0085397bd17111179c2a60f9fe6c3b71c458
pinjster <pinj2000@gmail.com>
2023-11-08 01:05:06 -0500

    added information/link to rMend project

--- app/applications/page.tsx
+++ app/applications/page.tsx
@@ -4,6 +4,9 @@ function page() {
   return (
     <div>
         <h1>My built applications</h1>
+        <h3>rMEND: Recommendation project</h3>
+        <p>Github link: <a href="https://github.com/pinjster/Capstone_Project">here</a>.</p>
+        <p>Hosted: <a href="https://rmend.netlify.app/">here</a>.</p>
     </div>
   )
 }
