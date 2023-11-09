fb0f1df41de79173059a97e935dee6c26d6942fe
pinjster <pinj2000@gmail.com>
2023-11-09 00:23:39 -0500

    included credly link

--- app/qualifactions/page.tsx
+++ app/qualifactions/page.tsx
@@ -5,7 +5,7 @@ function page() {
     <div>
         <h1>My Qualifications</h1>
         <h4>Arizona State University</h4>
-        <h4>Coding Temple</h4>
+        <h4>Coding Temple: <a href="https://www.credly.com/badges/36018540-8883-4adb-8bd8-e477830ada88/linked_in?t=rzra9e"> here</a></h4>
         <br />
         <h1>Working Experience</h1>
         <h4>Carvana</h4>
