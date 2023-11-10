c43b42b8b1239149168e68dca903a389d2885592
pinjster <pinj2000@gmail.com>
2023-11-10 02:53:41 -0500

    additional comments

--- app/qualifactions/page.tsx
+++ app/qualifactions/page.tsx
@@ -4,7 +4,7 @@ function page() {
   return (
     <div>
         <h1>My Qualifications</h1>
-        <h4>Arizona State University</h4>
+        <h4>Arizona State University - 1 year in Computer Science (BS)</h4>
         <h4>Coding Temple: <a href="https://www.credly.com/badges/36018540-8883-4adb-8bd8-e477830ada88/linked_in?t=rzra9e"> here</a></h4>
         <br />
         <h1>Working Experience</h1>
