d7fa82552869626c8df921cfe05d529746cb374a
pinjster <pinj2000@gmail.com>
2023-11-11 23:53:57 -0500

    carvana info added

--- app/qualifactions/page.tsx
+++ app/qualifactions/page.tsx
@@ -4,11 +4,12 @@ function page() {
   return (
     <div>
         <h1>My Qualifications</h1>
-        <h4>Arizona State University - 1 year in Computer Science (BS)</h4>
+        <h4>Arizona State University - 1 year in Computer Science (10/2021 - 10/2022)</h4>
         <h4>Coding Temple: <a href="https://www.credly.com/badges/36018540-8883-4adb-8bd8-e477830ada88/linked_in?t=rzra9e"> here</a></h4>
         <br />
         <h1>Working Experience</h1>
-        <h4>Carvana</h4>
+        <h4>Carvana - Logistics Team Lead (10/2020 - 08/2022)</h4>
+        <p></p>
     </div>
   )
 }
